09324ab6c119d37d492b14529f3cabced3a5c9c4
sammyreles22 <samuel.reles@gmail.com>
2020-02-08 20:05:35 -0500

    Added DjangoRestFramework and New Unit Test
    Added the first unit test for the User model, as well as added
    DjangoRestFramework for future API testing.

--- goalsetterbackend/comptroller/tests.py
+++ goalsetterbackend/comptroller/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from comptroller.models import User
+import json
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    GOALS = {
+        "Goals": {
+            "Goal": {
+                "Name": "Run a marathon.",
+                "Tasks Completed": "None",
+                "Description": "My plan is to run faster than everyone else.",
+                "Flat Goal": "Run a marathon in 20min.",
+                "Schedule": "None"
+            }
+        }
+    }
+
+    def setUp(self):
+        User.objects.create(email="user@email.com", password="12345", goals=json.dumps(self.GOALS))
+
+    def test_user_goal_is_to_run_a_marathon(self):
+        user = User.objects.get(email="user@email.com")
+        self.assertEqual(self.GOALS, user.get_goals())
