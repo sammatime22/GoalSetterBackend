4ef0bb09d564c653ddf72c97f53e0b2b9636929a
sammyreles22 <samuel.reles@gmail.com>
2020-02-14 21:46:28 -0500

    Removing Bad Practices
    Added in foreign keys for goals, tasks. Removed irrelevant code.

--- goalsetterbackend/comptroller/models.py
+++ goalsetterbackend/comptroller/models.py
@@ -9,36 +9,16 @@ class User(models.Model):
 
     @param email - The email of the user.
     @param password - The password of the user.
-    @param goals - A list of Goal objects that will be related to 
-                   the user.
     """
     email = models.EmailField()
     password = models.CharField(max_length=100)
-    goals = models.TextField()
-
-    def get_goals(self):
-        try:
-            return json.loads(self.goals)
-        # When the goals were set in a non-JSON way, there was probably a 
-        # incorrectly formatted request. For now, our solution is to set the
-        # goals field back to {"Goals": "None"}.
-        except ValueError:
-            self.set_goals({"Goals":"None"})
-            return json.loads(self.goals)
-
-    def set_goals(self, goal_update):
-        # Similarly, we will set the goals to {"Goals": "None"} if the value 
-        # provided wasn't JSON.
-        try:
-            self.goals = json.dumps(goal_update)
-        except ValueError:
-            self.goals = json.dumps({"Goals": "None"})
 
 
 class Goal(models.Model):
     """
     The definition of a Goal as precieved by the end user.
 
+    @param user - A key to a user object that has said goal.
     @param name - The name of the goal.
     @param tasks_completed - The number of tasks relative to the 
                              goal that have been completed.
@@ -46,32 +26,26 @@ class Goal(models.Model):
     @param flat_goal - The most basic definition of the goal.
     @param schedule - A list of Task objects related to the goal.
     """
+    user = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=200)
     tasks_completed = models.IntegerField()
     description = models.TextField()
     flat_goal = models.CharField(max_length=200)
-    # This is actually a list of Task objects, but is stored as a JSON string 
-    # inside the schedule field
-    schedule = models.TextField()
-
-    def get_shedule(self):
-        return json.loads(self.schedule)
-
-    def set_schedule(self, task_update):
-        self.schedule = json.dumps(task_update)
 
 
 class Task(models.Model):
     """
     The definition of a Task as based on a Goal.
 
+    @param goal - The goal that this task is related to.
     @param time - The time the task is to be/was completed.
     @param completed - A boolean representing if the task is done.
     @name - The name of the task.
     @related_goal - The goal that this task is related to.
     @notes - Notes pertaining to the task.
     """
-    time = models.DateField()
+    goal = models.ForeignKey('Goal', on_delete=models.CASCADE, default=None)
+    time = models.DateTimeField()
     completed = models.BooleanField(default=False)
     name = models.CharField(max_length=200)
     related_goal = models.CharField(max_length=200)
--- goalsetterbackend/comptroller/tests.py
+++ goalsetterbackend/comptroller/tests.py
@@ -4,35 +4,12 @@ import json
 
 # Create your tests here.
 class UserTestCase(TestCase):
-    # A Mock Goals JSON Object
-    GOALS = {
-        "Goals": {
-            "Goal": {
-                "Name": "Run a marathon.",
-                "Tasks Completed": "None",
-                "Description": "My plan is to run faster than everyone else.",
-                "Flat Goal": "Run a marathon in 20min.",
-                "Schedule": "None"
-            }
-        }
-    }
 
     user = None
 
     def setUp(self):
-        User.objects.create(email="user@email.com", password="12345", goals=\
-            json.dumps(self.GOALS))
+        User.objects.create(email="user@email.com", password="12345")
         self.user = User.objects.get(email="user@email.com")
 
     def test_00_user_password_checks_out(self):
         self.assertEqual("12345", self.user.password)
-
-    def test_01_user_goal_is_to_run_a_marathon(self):
-        self.assertEqual(self.GOALS, self.user.get_goals())
-
-    def test_02_user_goal_not_written_in_json_becomes_nothing(self):
-        User.objects.create(email="baduser@bad.com", password="54321", goals=\
-            "Eat a cherry pie.")
-        bad_user = User.objects.get(email="baduser@bad.com")
-        self.assertEqual({"Goals": "None"}, bad_user.get_goals())
-
