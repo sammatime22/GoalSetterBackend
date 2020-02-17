ebc42be2b72612a7bf8d9ea69eb2533670b7975f
sammyreles22 <samuel.reles@gmail.com>
2020-02-16 22:02:59 -0500

    Added first endpoint
    Added first endpoint with CRUD operations for Users.

--- goalsetterbackend/goalsetterbackend/urls.py
+++ goalsetterbackend/goalsetterbackend/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from rest_framework import routers
+from comptroller import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView, 'user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authenticate/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('api/', include(router.urls))
 ]
